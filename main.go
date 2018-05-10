83fc5a74bd0087b70b9f36d42c7ea4d280c34338
jinzzam <firstmind_@naver.com>
2018-05-10 15:32:43 +0900

    add comments, for loop

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 	"math"
 )
 
-const (
+const ( //상수 : 문자, 문자열, boolean, 숫자 타입 중 하나
+	//타입을 지정해주지 않은 상수는 문맥에 따라 타입을 자동으로 가짐
 	v = 122
 	w = v + 4
 )
@@ -24,13 +25,18 @@ func main() {
 	d = append(d)
 	printSlice("d", d)
 
-	var x, y, z int = 1, 2, 3
-	C, python, java := true, false, "no!"
+	var x, y, z int = 1, 2, 3             /*변수 선언*/
+	C, python, java := true, false, "no!" //:=를 사용하면 var과 명시적입 타입 생략 가능
 	fmt.Println(x, y, z, C, python, java)
 	fmt.Println(v, w)
 
 	fmt.Println(pow(3, 3, 20), pow(3, 2, 10))
 
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println("0~10 sum : ", sum)
 }
 
 func printSlice(s string, x []int) {
