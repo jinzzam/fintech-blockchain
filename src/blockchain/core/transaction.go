cbc1706e21d265cda4f960c857059ca70b146cae
jinzzam <firstmind_@naver.com>
2018-05-10 17:43:11 +0900

    add transaction

--- src/blockchain/core/transaction.go
+++ src/blockchain/core/transaction.go
@@ -1,4 +1,23 @@
 package core
 
+import "fmt"
+
+type Address [20]byte
+
 type Transaction struct { //블록 내용 (거래기록 등)
+	From Address
+	Data txdata
+}
+
+func (a Address) ToString() string {
+	res := "0x"
+	for _, v := range a {
+		res += fmt.Sprintf("%02x", v)
+	}
+	return res
+}
+
+type txdata struct {
+	To     Address
+	Amount uint64
 }
