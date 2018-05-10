164c0f51fdf2290f07122c22ca5162cd5dd13644
jinzzam <firstmind_@naver.com>
2018-05-10 17:08:41 +0900

    add blockchain

---
+++ src/blockchain/core/blockchain.go
@@ -0,0 +1,44 @@
+package core
+
+var GlobalBlockChains []*BlockChain
+
+type Blockchain struct {//블록체인 구조
+	Blocks           []Block
+	BlockChainHeight uint64
+	GenesisBlock     *Block
+	CandidateBlock   *Block
+}
+
+func NewBlockChain() *BlockChain {//블록체인 생성
+	b := newGenesisBlock()
+	bc := &Blockchain{
+		Blocks:           []Block{*b},
+		BlockChainHeight: 1,
+		GenesisBlock:     b,
+		CandidateBlock:   nil,
+	}
+	return bc
+}
+
+func newGenesisBlock() *Block{ //제네시스 블록 생성
+  b := &Block{
+    Header: BlockHeader{
+      PreviousHash: Sha256.Sum256([]byte{}),
+      Timestamp : time.Now().UnixNano(),
+      Index : 0,
+    },
+    Body: BlockBody{}
+  }
+  return b
+}
+
+func (bc *Blockchain) AddBlock() error{//후보 블록 생성
+  bc.Blocks = append(bc.Blocks, *bc.CandidateBlock)
+  bc.BlockChainHeight = bc.BlockChainHeight + 1
+  bc.CandidateBlock = nil
+  return nil
+}
+
+func AppendBlockchain(bc *Blockchain)  {  //블록체인 발견 시 추가
+
+}
