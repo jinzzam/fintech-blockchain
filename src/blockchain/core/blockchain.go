46dd29a129807d3699d4918b99acea7e4ea322fd
jinzzam <firstmind_@naver.com>
2018-05-11 11:09:12 +0900

    edit blockchain

--- src/blockchain/core/blockchain.go
+++ src/blockchain/core/blockchain.go
@@ -1,15 +1,20 @@
 package core
 
-var GlobalBlockChains []*BlockChain
+import (
+	"crypto/sha256"
+	"time"
+)
 
-type Blockchain struct {//블록체인 구조
+var GlobalBlockChains []*Blockchain
+
+type Blockchain struct { //블록체인 구조
 	Blocks           []Block
 	BlockChainHeight uint64
 	GenesisBlock     *Block
 	CandidateBlock   *Block
 }
 
-func NewBlockChain() *BlockChain {//블록체인 생성
+func NewBlockChain() *Blockchain { //블록체인 생성
 	b := newGenesisBlock()
 	bc := &Blockchain{
 		Blocks:           []Block{*b},
@@ -20,26 +25,26 @@ func NewBlockChain() *BlockChain {//블록체인 생성
 	return bc
 }
 
-func newGenesisBlock() *Block{ //제네시스 블록 생성
-  b := &Block{
-    Header: BlockHeader{
-      PreviousHash: sha256.Sum256([]byte{}),
-      Timestamp : time.Now().UnixNano(),
-      Index : 0,
-    },
-    Body: BlockBody{}
-  }
-  return b
+func newGenesisBlock() *Block { //제네시스 블록 생성
+	b := &Block{
+		Header: BlockHeader{
+			PreviousHash: sha256.Sum256([]byte{}),
+			Timestamp:    time.Now().UnixNano(),
+			Index:        0,
+		},
+		Body: BlockBody{},
+	}
+	return b
 }
 
-func (bc *Blockchain) AddBlock() error{//후보 블록 생성
-  bc.Blocks = append(bc.Blocks, *bc.CandidateBlock)
-  bc.BlockChainHeight = bc.BlockChainHeight + 1
-  bc.CandidateBlock = nil
-  return nil
+func (bc *Blockchain) AddBlock() error { //후보 블록 생성
+	bc.Blocks = append(bc.Blocks, *bc.CandidateBlock)
+	bc.BlockChainHeight = bc.BlockChainHeight + 1
+	bc.CandidateBlock = nil
+	return nil
 }
 
-func AppendBlockchain(bc *Blockchain)  {  //블록체인 발견 시 추가
-  GlobalBlockChain = append(GlobalBlockChain, bc)
-  return nil
+func AppendBlockchain(bc *Blockchain) { //블록체인 발견 시 추가
+	GlobalBlockChain = append(GlobalBlockChain, bc)
+	return nil
 }
